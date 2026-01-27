2ed12be79ddf1556336f7e01ad5749caebdf5713
a-319 <5409292@gmail.com>
2026-01-27 22:03:52 +0200

    Add imports for DevicePolicyManager and ComponentName
    Added imports for DevicePolicyManager and ComponentName.

--- app/src/main/java/com/aurora/store/data/receiver/DeviceOwnerReceiver.kt
+++ app/src/main/java/com/aurora/store/data/receiver/DeviceOwnerReceiver.kt
@@ -21,6 +21,8 @@
 package com.aurora.store.data.receiver
 
 import android.app.admin.DeviceAdminReceiver
+import android.app.admin.DevicePolicyManager
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.os.PersistableBundle
